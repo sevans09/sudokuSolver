4acf5cde8bb8ace6c4971f1d40639a7670045dcc
Sook-Hee Evans <evanssookhee@gmail.com>
2019-10-17 11:05:32 -0400

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -10,7 +10,26 @@ def dups_in_box:
 
 
 def main():
-  print("Initial board: ", board)
+	# expert board
+	board = [
+	    [8,0,0,0,0,0,0,0,0],
+	    [0,0,3,6,0,0,0,0,0],
+	    [0,7,0,0,9,0,2,0,0],
+	    [0,5,0,0,0,7,0,0,0],
+	    [0,0,0,0,4,5,7,0,0],
+	    [0,0,0,1,0,0,0,3,0],
+	    [0,0,1,0,0,0,0,6,8],
+	    [0,0,8,5,0,0,0,1,0],
+	    [0,9,0,0,0,0,4,0,0]]
+
+	print("Initial board: ", board)
+	
+	mySudoku = Sudoku( board )
+	start = time.time()
+
+	
+	print( "Solved in {} seconds".format( time.time() - start ) )
+ 	
 
 
 if __name__== "__main__":
